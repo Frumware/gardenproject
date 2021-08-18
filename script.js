b84e289240cc30065823d559b3d945dbf126124c
Camilo Casanova <camcas1107@gmail.com>
2021-08-18 14:22:35 -0400

    bruh

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-Links");
 const links = document.querySelectorAll(".nav-Links li");
@@ -9,3 +11,25 @@ hamburger.addEventListener("click", () => {
 });
 });
 
+let url = "https://www.fruityvice.com/api/fruit/banana";
+
+fetch(url).then(
+    function(response){
+        return response.json();
+    }
+).then(
+    function(json){
+        let example = document.createElement("h3");
+        
+        
+        example.innerHTML = json.name;
+        
+        
+        
+        example.style["font-family"] = "Garamond";
+        description.innerHTML = json.desc[0];
+        document.body.appendChild(example);
+        
+        console.log(json);
+    }
+)
