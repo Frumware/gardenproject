df65d963ff74772aec47755fef80a5d5018fbca6
Camilo Casanova <camcas1107@gmail.com>
2021-08-19 13:53:43 -0400

    firstcommit

--- script.js
+++ script.js
@@ -11,25 +11,70 @@ hamburger.addEventListener("click", () => {
 });
 });
 
-let url = "https://www.fruityvice.com/api/fruit/banana";
+let url = "https://www.fruityvice.com/api/fruit/strawberry";
 
 fetch(url).then(
     function(response){
+        console.log(response);
         return response.json();
     }
 ).then(
     function(json){
-        let example = document.createElement("h3");
+        
+        let food = document.createElement("h1");
+        let family = document.createElement("h3");
+        let nutritions = document.createElement("h3");
+        let carbs = document.createElement("h3")
+        let sugar = document.createElement("h3")
+        let protein = document.createElement("h3")
+        let fat = document.createElement("h3")
         
         
-        example.innerHTML = json.name;
         
         
         
-        example.style["font-family"] = "Garamond";
-        description.innerHTML = json.desc[0];
-        document.body.appendChild(example);
+        
+        food.innerHTML = json.name;
+        family.innerHTML = "Food Family: " + json.family;
+        nutritions.innerHTML = "Calories: " + json.nutritions.calories + "g";
+        carbs.innerHTML = "Carbohydrates: " + json.nutritions.carbohydrates + "g";
+        sugar.innerHTML = "Sugar: " + json.nutritions.sugar + "g";
+        protein.innerHTML = "Protein: " + json.nutritions.protein + "g";
+        fat.innerHTML = "Fat: " + json.nutritions.fat + "g";
+
+
+
+        
+        
+        
+        
+        
+        food.style["font-family"] = "Garamond";
+        family.style["font-family"] = "Garamond";
+        nutritions.style["font-family"] = "Garamond";
+        carbs.style["font-family"] = "Garamond";
+        sugar.style["font-family"] = "Garamond";
+        protein.style["font-family"] = "Garamond";
+        fat.style["font-family"] = "Garamond";
+        
+        
+       
+        
+        
+        
+        document.body.appendChild(food);
+        document.body.appendChild(family);
+        document.body.appendChild(nutritions);
+        document.body.appendChild(carbs);
+        document.body.appendChild(sugar);
+        document.body.appendChild(protein);
+        document.body.appendChild(fat);
+        
+       
+        
+        
+        
         
         console.log(json);
     }
-)
+) 
